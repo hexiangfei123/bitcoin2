18437f41ff0e992f9097dca1399c6f880cc7d590
贺向飞 <1132310433@qq.com>
2019-11-13 18:41:02 +0800

    创建项目

---
+++ bitcoit/src/main/java/com/hxf/bitcoit/BitcoitApplication.java
@@ -0,0 +1,13 @@
+package com.hxf.bitcoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitcoitApplication.class, args);
+    }
+
+}
